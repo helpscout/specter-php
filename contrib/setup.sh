a204cc30060413f71a9017bccf568f502c3a41f6
Craig Davis <craig@helpscout.com>
2016-04-30 19:14:35 -0600

    Add pre-commit hooks and composer post install to setup

---
+++ contrib/setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Help Scout Contribution Tooling
+# ==============================================================================
+# Setup our pre-commit hooks if the tool is available
+#
+command -v pre-commisdt >/dev/null 2>&1 || {
+    echo ""
+    echo "We'd like for you to consider running our pre-commit hooks. To do so, you'll"
+    echo "need to install the pre-commit tool from http://pre-commit.com/"
+    echo "and then run 'pre-commit install' in this directory. Check out the"
+    echo ".pre-commit-config.yaml file to learn more about our QA checks."
+    exit 0;
+}
+echo ""
+echo "Thank you for running pre-commit!. Installing the Help Scout Hooks"
+echo "for this repo."
+echo ""
+pre-commit install
