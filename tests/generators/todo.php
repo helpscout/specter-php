48a76d5b7484375b16f61a68cc947296fad0ad6d
Craig Davis <craig@there4development.com>
2017-05-17 21:33:05 -0600

    Add non-functional code to the fixture generator tool

---
+++ tests/generators/todo.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Fixture configuration for Todos
+ */
+return [
+    'outputDirectory' => './examples/fixtures',
+    'specterDirectory' => './test/fixture/todo',
+    'specs' => [
+        [
+            'specterFile' => 'todo.json',
+            'outputFile'  => 'todo-simple.json',
+            'seed'        => 1,
+            'description' => 'A simple todo example',
+        ],
+        [
+            'specterFile' => 'todo.json',
+            'outputFile'  => 'todo-no-title.json',
+            'seed'        => 2,
+            'postProcess' => function ($fixture) {
+                $fixture->title = '';
+            },
+            'description' => 'A todo without a title',
+        ],
+        [
+            'specterFile' => 'todo.json',
+            'outputFile'  => 'todo-with-integer-id.json',
+            'seed'        => 3,
+            'postProcess' => function ($fixture) {
+                $fixture->id = 45678;
+            },
+            'description' => 'A todo with an integer id instead of a uuid',
+        ]
+    ]
+];
